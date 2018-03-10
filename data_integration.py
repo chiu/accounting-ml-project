a4be7f177f322fa1d32d8cbe58a40b0415c53342
vlc4 <vlc4@sfu.ca>
2018-03-09 17:01:24 -0800

    minor fix

--- data_integration.py
+++ data_integration.py
@@ -66,7 +66,7 @@ except:
     pass
 
 try:
-    final_joined.write.parquet('/user/vcs/annual_integrated_dataset_parquet')
+    final_joined.write.parquet('/user/vcs/annual_integrated_dataset.parquet')
 except:
     pass
 
