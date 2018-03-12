fb80f1dcdda60654eec52548b1864d36240b14f3
vlc4 <vlc4@sfu.ca>
2018-03-11 21:35:53 -0700

    added code for 1hot

--- load_parquet.py
+++ load_parquet.py
@@ -27,3 +27,27 @@ sc = spark.sparkContext
 integrated_df = spark.read.parquet('/user/vcs/annual_integrated_dataset_v2.parquet')
 print(integrated_df.head())
 print('v4 encoded')
+
+
+
+
+permuted_annual_dtypes = integrated_df.dtypes
+
+
+# In[14]:
+
+
+string_columns = [k for (k,v) in permuted_annual_dtypes if v == 'string']
+df = integrated_df.cache()
+
+for string_column in string_columns:
+    stringIndexer = StringIndexer(inputCol=string_column, outputCol="categoryIndex")
+    model = stringIndexer.fit(df)
+    indexed = model.transform(df)
+
+    encoder = OneHotEncoder(inputCol="categoryIndex", outputCol= string_column+"Vec")
+    encoded = encoder.transform(indexed)
+    df = encoded
+
+df.head()
+
