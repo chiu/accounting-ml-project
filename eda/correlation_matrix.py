29dc3a59264a6fb108d9cce8329e917546836b4c
vlc4 <vlc4@sfu.ca>
2018-03-17 18:56:19 -0700

    done correlation matrix

--- eda/correlation_matrix.py
+++ eda/correlation_matrix.py
@@ -65,46 +65,28 @@ output = assembler.transform(df)
 
 output.head()
 
-# from pyspark.sql import SparkSession
-# from pyspark.sql.functions import isnan, when, count, col
-# spark = SparkSession.builder.appName('733').getOrCreate()
-# sc = spark.sparkContext
-#
-# """
-# An example for computing correlation matrix.
-# Run with:
-#   bin/spark-submit examples/src/main/python/ml/correlation_example.py
-# """
-# from __future__ import print_function
-#
-# # $example on$
-# from pyspark.ml.linalg import Vectors
-# from pyspark.ml.stat import Correlation
-# # $example off$
-# from pyspark.sql import SparkSession
-#
-# # if __name__ == "__main__":
-# #     spark = SparkSession \
-# #         .builder \
-# #         .appName("CorrelationExample") \
-# #         .getOrCreate()
-#
-#     # # $example on$
-#     # data = [(Vectors.sparse(4, [(0, 1.0), (3, -2.0)]),),
-#     #         (Vectors.dense([4.0, 5.0, 0.0, 3.0]),),
-#     #         (Vectors.dense([6.0, 7.0, 0.0, 8.0]),),
-#     #         (Vectors.sparse(4, [(0, 9.0), (3, 1.0)]),)]
-#     # df = spark.createDataFrame(data, ["features"])
-#
-#     df = integrated_df
-#
-#     r1 = Correlation.corr(df, "features").head()
-#     print("Pearson correlation matrix:\n" + str(r1[0]))
-#
-#     r2 = Correlation.corr(df, "features", "spearman").head()
-#     print("Spearman correlation matrix:\n" + str(r2[0]))
-#     # $example off$
-#
-#     spark.stop()
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import isnan, when, count, col
+spark = SparkSession.builder.appName('733').getOrCreate()
+sc = spark.sparkContext
+
+"""
+An example for computing correlation matrix.
+Run with:
+  bin/spark-submit examples/src/main/python/ml/correlation_example.py
+"""
+from __future__ import print_function
+
+from pyspark.ml.stat import Correlation
+df = output
+
+r1 = Correlation.corr(df, "features").head()
+print("Pearson correlation matrix:\n" + str(r1[0]))
+
+r2 = Correlation.corr(df, "features", "spearman").head()
+print("Spearman correlation matrix:\n" + str(r2[0]))
+# $example off$
+
+spark.stop()
 
 
