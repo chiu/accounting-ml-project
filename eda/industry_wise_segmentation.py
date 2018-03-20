1287d1949f12190c2d6afe0ed02b7afdcdee4b29
vishukla <32084342+vishukla@users.noreply.github.com>
2018-03-20 12:24:46 -0700

    Add files via upload

---
+++ eda/industry_wise_segmentation.py
@@ -0,0 +1,30 @@
+'''
+	Script to analyse the number of financial statemets segmented according to industry segments.
+'''
+
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName('733').getOrCreate()
+
+annual_compustat = spark.read.csv('/user/vcs/annual_compustat.csv', header=True, inferSchema=True)
+siccodes = spark.read.csv('/user/vcs/siccodes.csv', header=True, inferSchema=False)
+dgls = spark.read.csv('/user/vcs/dgls_integrated.csv', header=True, inferSchema=True)
+
+ls = ['SIC', 'CONM', 'ACCHG', 'ACO', 'ACOMINC', 'ACQCSHI', 'ACQGDWL', 'ACQINTAN', 'ACQLNTAL', 'ACT', 'AMGW','AQ','AP','AQC','ARC','AT','AU','AUOP','AUOPIC','BKVLPS','CAPX','CDVC','CEQ','CGA','CHE','CHECH','CI','CIMII','CLD2','CLD3','CLD4','CLD5','CLT','COGS','CSHI','CURNCD','DCOM','DCPSTK','DCVT','DEPC','DLTT','DM','DO','DP','DPACLS','DRC','DRLT','DV','DVC','DVPA','DVPDP','DVPSP_F','EBIT','EBITDA','EMOL','EMP','EPSFI','EPSPI','ESOPCT','ESOPDLT','EXCHG','EXRE','FATB','FATE','FATN','FATL','FATO','FCA','FEL','FIC','FINCF','FOPT','FYEAR','FYR','GDWL','GDWLAM','GDWLIA','GLA','GP','GWO','HEDGEGL','ICAPT','IDBFLAG','IDIIS','IDILC','IDIT','INTAN','INTC','INVCH','INCFG','INVOFS','INVRM','INVT','INVWIP','IPODATE','ISEQ','ISEQC','ISEQM','ISFI','ISGR','ISGU','ITCB','IVCH','IVNCF','LCT','LCUACU','LIFR','LLRCI','LLWOCI','LT','MIB','MIBT','MII','MKVALT','NAICS','NI','NIINT','NIINTPFC','NIINTPFP','NOPIO','NP','NPANL','NPAT','NRTXT','OANCF','OB','OIADP','OIBDP','OPEPS','OPINI','OPITI','OPREPSX','OPTCA','OPTEX','OPTEXD','OPTFVGR','OPTGR','OPTPRCCA','OPTPRCEX','OPTPRCEY','OPTPRCGR','OPTPRCWA','OPTVOL','PCL','PI','PIFO','PLL','PNCA','PNCIA','PPEGT','PPENB','PPENC','PPENLS','PPENNR','PPENT','PPEVO','PPEVR','PRCC_F','PRCH_F','PRICAN','PRIROW','PRIUSA','PRODV','PRSTKCC','PRSTKPC','PSTKC','PVCL','PVO','PVPL','PVT','RANK','RCA','RDIP','RE','REA','REAJO','RECCH','RECT','RECTA','REUNA','REVT','RLL','RMUM','RPAG','RVLRV','RVTI','SALE','SALEPFC','SALEPFP','SCO','SCSTKC','SEQ','SIC','SIV','SPCE','SPCINDCD','SPCSECCD','SPCSRC','SPI','SPPE','SPPIV','SPSTKC','SRET','SSNP','STBO','STIO','STKCO','STKO','TDC','TEQ','TFVA','TFVCE','TFVL','TIC','TLCF','TRANSA','TSA','TSTKC','TSTKP','TXC','TXDB','TXDBA','TXDBCA','TXDBCL','TXFED','TXFO','TXPD','TXR','TXT','TXTUBXINTBS','TXTUBXINTIS','VPAC','VPO','WCAP','WCAPC','WCAPCH','WDA','XACC','XAD','XAGO','XAGT','XCOM','XDP','XEQO','XI','XIDO','XINST','XINT','XINTD','XINTOPT','XLR','XOPR','XOPRAR','XPP','XPR','XRD','XRENT','XS','XSGA','XSTF','XSTFO','XSTFWS','XT']
+
+ls = [x.lower() for x in ls]
+
+annual_filtered = annual_compustat.select([c for c in annual_compustat.columns if c in ls])
+annual_filtered = annual_filtered.dropDuplicates(['tic'])
+annual_joined = annual_filtered.join(dgls, dgls['COMPUSTAT IDENTIFIER'] == annual_filtered['conm'],'right')
+annual_joined = annual_joined.dropDuplicates(['ID'])
+
+sic_joined_df = annual_joined.join(siccodes, siccodes['SIC Code'] == annual_joined['sic'], 'right')
+joined_df = annual_filtered.join(siccodes, siccodes['SIC Code'] == annual_filtered['sic'], 'right')
+
+total_sic_counts = joined_df.groupBy('sic').count()
+total_sic_counts.coalesce(1).write.csv('total_industrywise_counts', header=True)
+
+misstated_sic_counts = sic_joined_df.groupBy('sic').count()
+misstated_sic_counts.coalesce(1).write.csv('misstated_industrywise_counts', header=True)
